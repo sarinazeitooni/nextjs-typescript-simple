31fec22689f5f6a2e0186f4de1e8de9c91338cd8
sarinazeitooni <zeitoonisarina@gmail.com>
2021-07-28 10:41:56 +0430

    added sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import style from './styles/index.module.scss';
 const Index: React.FunctionComponent = () => {
     return (
         <Layout title="Home">
-            <h1>Hello Next.js 👋</h1>
+            <h1 className={style['test']}>Hello Next.js 👋</h1>
         </Layout>
     )
 }
